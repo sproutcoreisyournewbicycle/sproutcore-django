e615252f001d189b1aa9320fe0b81da2efc06f1c
Erich Ocean <erich.ocean@me.com>
2009-09-11 17:59:39 -0700

    initial commit of Django.*Field tranformers

---
+++ core.js
@@ -0,0 +1,52 @@
+Django = SC.Object.extend() ;
+
+Django.Field = SC.Object.extend() ;
+
+// define the Django field type tags (used in SC.RecordAttribute transforms)...
+Django.AutoField = Django.Field.extend() ;
+Django.BooleanField = Django.Field.extend() ;
+Django.CharField = Django.Field.extend() ;
+Django.CommaSeparatedIntegerField = Django.Field.extend() ;
+Django.DateField = Django.Field.extend() ;
+Django.DateTimeField = Django.Field.extend() ;
+Django.DecimalField = Django.Field.extend() ;
+Django.EmailField = Django.Field.extend() ;
+Django.FileField = Django.Field.extend() ;
+Django.FilePathField = Django.Field.extend() ;
+Django.FloatField = Django.Field.extend() ;
+Django.ImageField = Django.Field.extend() ;
+Django.IntegerField = Django.Field.extend() ;
+Django.IPAddressField = Django.Field.extend() ;
+Django.NullBooleanField = Django.Field.extend() ;
+Django.PositiveIntegerField = Django.Field.extend() ;
+Django.PositiveSmallIntegerField = Django.Field.extend() ;
+Django.SlugField = Django.Field.extend() ;
+Django.SmallIntegerField = Django.Field.extend() ;
+Django.TextField = Django.Field.extend() ;
+Django.TimeField = Django.Field.extend() ;
+Django.URLField = Django.Field.extend() ;
+Django.XMLField = Django.Field.extend() ;
+
+/** @private Taken from http://www.devarticles.com/c/a/JavaScript/JavaScript-and-XML/2/ */
+Django.parseXML = function(text) {
+    if (typeof DOMParser != "undefined") {
+        // Mozilla, Firefox, and related browsers
+        return (new DOMParser()).parseFromString(text, "application/xml");
+    }
+    else if (typeof ActiveXObject != "undefined") { 
+        // Internet Explorer.
+        var doc = XML.newDocument( );   // Create an empty document
+        doc.loadXML(text);              //  Parse text into it
+        return doc;                     // Return it
+    }
+    else {
+        // As a last resort, try loading the document from a data: URL
+        // This is supposed to work in Safari. Thanks to Manos Batsis and
+        // his Sarissa library (sarissa.sourceforge.net) for this technique.
+        var url = "data:text/xml;charset=utf-8," + encodeURIComponent(text);
+        var request = new XMLHttpRequest();
+        request.open("GET", url, false);
+        request.send(null);
+        return request.responseXML;
+    }
+};
---
+++ models/record_attribute.js
@@ -0,0 +1,382 @@
+/** @private */
+SC.RecordAttribute.registerTransform(Django.AutoField, {
+  
+  /** @private - convert an auto field to an integer */
+  to: function(obj) {
+    return SC.none(obj) ? null : Number(obj) ;
+  }
+  
+});
+
+/** @private */
+SC.RecordAttribute.registerTransform(Django.BooleanField, {
+  
+  /** @private - convert a boolean field to a boolean */
+  to: function(obj) {
+    return !!obj ;
+  }
+  
+});
+
+/** @private */
+SC.RecordAttribute.registerTransform(Django.CharField, {
+  
+  /** @private - 
+    convert a char field to a string 
+    allow null through as that will be checked separately
+  */
+  to: function(obj) {
+    if (!(typeof obj === SC.T_STRING) && !SC.none(obj) && obj.toString) {
+      obj = obj.toString();
+    }
+    return obj;
+  }
+  
+});
+
+/** @private */
+SC.RecordAttribute.registerTransform(Django.CommaSeparatedIntegerField, {
+  
+  /** @private - 
+    convert a comma-separated integer field to a string 
+    allow null through as that will be checked separately
+  */
+  to: function(obj) {
+    if (!(typeof obj === SC.T_STRING) && !SC.none(obj) && obj.toString) {
+      obj = obj.toString();
+    }
+    return obj;
+  }
+  
+});
+
+/** @private */
+SC.RecordAttribute.registerTransform(Django.DateTimeField, {
+
+  /** @private - convert a string to a Date */
+  to: function(str, attr) {
+    var ret ;
+    
+    if (attr.get('useIsoDate')) {
+      var regexp = "([0-9]{4})(-([0-9]{2})(-([0-9]{2})" +
+             "(T([0-9]{2}):([0-9]{2})(:([0-9]{2})(\\.([0-9]+))?)?" +
+             "(Z|(([-+])([0-9]{2}):([0-9]{2})))?)?)?)?",
+          d      = str.match(new RegExp(regexp)),
+          offset = 0,
+          date   = new Date(d[1], 0, 1),
+          time ;
+
+      if (d[3]) { date.setMonth(d[3] - 1); }
+      if (d[5]) { date.setDate(d[5]); }
+      if (d[7]) { date.setHours(d[7]); }
+      if (d[8]) { date.setMinutes(d[8]); }
+      if (d[10]) { date.setSeconds(d[10]); }
+      if (d[12]) { date.setMilliseconds(Number("0." + d[12]) * 1000); }
+      if (d[14]) {
+         offset = (Number(d[16]) * 60) + Number(d[17]);
+         offset *= ((d[15] == '-') ? 1 : -1);
+      }
+
+      offset -= date.getTimezoneOffset();
+      time = (Number(date) + (offset * 60 * 1000));
+      
+      ret = new Date();
+      ret.setTime(Number(time));
+    } else ret = new Date(Date.parse(str));
+    return ret ;
+  },
+  
+  _dates: {},
+
+  _zeropad: function(num) { 
+    return ((num<0) ? '-' : '') + ((num<10) ? '0' : '') + Math.abs(num); 
+  },
+  
+  /** @private - convert a date to a string */
+  from: function(date) { 
+    var ret = this._dates[date.getTime()];
+    if (ret) return ret ; 
+    
+    // figure timezone
+    var zp = this._zeropad,
+        tz = 0-date.getTimezoneOffset()/60;
+        
+    tz = (tz === 0) ? 'Z' : '%@:00'.fmt(zp(tz));
+    
+    this._dates[date.getTime()] = ret = "%@-%@-%@T%@:%@:%@%@".fmt(
+      zp(date.getFullYear()),
+      zp(date.getMonth()+1),
+      zp(date.getDate()),
+      zp(date.getHours()),
+      zp(date.getMinutes()),
+      zp(date.getSeconds()),
+      tz) ;
+    
+    return ret ;
+  }
+});
+
+/** @private */
+SC.RecordAttribute.registerTransform(Django.DecimalField, {
+  
+  /** @private - convert a decimal field to a Number */
+  to: function(obj) {
+    return SC.none(obj) ? null : Number(obj) ;
+  }
+  
+});
+
+/** @private */
+SC.RecordAttribute.registerTransform(Django.FileField, {
+  
+  /** @private - 
+    convert a file field to a string 
+    allow null through as that will be checked separately
+  */
+  to: function(obj) {
+    if (!(typeof obj === SC.T_STRING) && !SC.none(obj) && obj.toString) {
+      obj = obj.toString();
+    }
+    return obj;
+  }
+  
+});
+
+/** @private */
+SC.RecordAttribute.registerTransform(Django.FilePathField, {
+  
+  /** @private - 
+    convert a file path field to a string 
+    allow null through as that will be checked separately
+  */
+  to: function(obj) {
+    if (!(typeof obj === SC.T_STRING) && !SC.none(obj) && obj.toString) {
+      obj = obj.toString();
+    }
+    return obj;
+  }
+  
+});
+
+/** @private */
+SC.RecordAttribute.registerTransform(Django.FloatField, {
+  
+  /** @private - convert a float field to a Number */
+  to: function(obj) {
+    return SC.none(obj) ? null : Number(obj) ;
+  }
+  
+});
+
+/** @private */
+SC.RecordAttribute.registerTransform(Django.ImageField, {
+  
+  /** @private - 
+    convert an image field to a string 
+    allow null through as that will be checked separately
+  */
+  to: function(obj) {
+    if (!(typeof obj === SC.T_STRING) && !SC.none(obj) && obj.toString) {
+      obj = obj.toString();
+    }
+    return obj;
+  }
+  
+});
+
+/** @private */
+SC.RecordAttribute.registerTransform(Django.IntegerField, {
+  
+  /** @private - convert an integere field to a Number */
+  to: function(obj) {
+    return SC.none(obj) ? null : Number(obj) ;
+  }
+  
+});
+
+/** @private */
+SC.RecordAttribute.registerTransform(Django.IPAddressField, {
+  
+  /** @private - 
+    convert a IP address field to a string 
+    allow null through as that will be checked separately
+  */
+  to: function(obj) {
+    if (!(typeof obj === SC.T_STRING) && !SC.none(obj) && obj.toString) {
+      obj = obj.toString();
+    }
+    return obj;
+  }
+  
+});
+
+/** @private */
+SC.RecordAttribute.registerTransform(Django.NullBooleanField, {
+  
+  /** @private - convert a null boolean field to a boolean */
+  to: function(obj) {
+    return (obj === null) ? null : !!obj ;
+  }
+  
+});
+
+/** @private */
+SC.RecordAttribute.registerTransform(Django.PositiveIntegerField, {
+  
+  /** @private - convert a positive integer field to a Number */
+  to: function(obj) {
+    return SC.none(obj) ? null : Number(obj) ;
+  }
+  
+});
+
+/** @private */
+SC.RecordAttribute.registerTransform(Django.PositiveSmallIntegerField, {
+  
+  /** @private - convert a positive small integer field to a Number */
+  to: function(obj) {
+    return SC.none(obj) ? null : Number(obj) ;
+  }
+  
+});
+
+/** @private */
+SC.RecordAttribute.registerTransform(Django.SlugField, {
+  
+  /** @private - 
+    convert a slug field to a string 
+    allow null through as that will be checked separately
+  */
+  to: function(obj) {
+    if (!(typeof obj === SC.T_STRING) && !SC.none(obj) && obj.toString) {
+      obj = obj.toString();
+    }
+    return obj;
+  }
+  
+});
+
+/** @private */
+SC.RecordAttribute.registerTransform(Django.SmallIntegerField, {
+  
+  /** @private - convert a small integer field to a Number */
+  to: function(obj) {
+    return SC.none(obj) ? null : Number(obj) ;
+  }
+  
+});
+
+/** @private */
+SC.RecordAttribute.registerTransform(Django.TextField, {
+  
+  /** @private - 
+    convert a slug field to a string 
+    allow null through as that will be checked separately
+  */
+  to: function(obj) {
+    if (!(typeof obj === SC.T_STRING) && !SC.none(obj) && obj.toString) {
+      obj = obj.toString();
+    }
+    return obj;
+  }
+  
+});
+
+/** @private FIXME: How does Django pass times in JSON? */
+SC.RecordAttribute.registerTransform(Django.TimeField, {
+
+  /** @private - convert a string to a Date */
+  to: function(str, attr) {
+    var ret ;
+    
+    if (attr.get('useIsoDate')) {
+      var regexp = "([0-9]{4})(-([0-9]{2})(-([0-9]{2})" +
+             "(T([0-9]{2}):([0-9]{2})(:([0-9]{2})(\\.([0-9]+))?)?" +
+             "(Z|(([-+])([0-9]{2}):([0-9]{2})))?)?)?)?",
+          d      = str.match(new RegExp(regexp)),
+          offset = 0,
+          date   = new Date(d[1], 0, 1),
+          time ;
+
+      if (d[3]) { date.setMonth(d[3] - 1); }
+      if (d[5]) { date.setDate(d[5]); }
+      if (d[7]) { date.setHours(d[7]); }
+      if (d[8]) { date.setMinutes(d[8]); }
+      if (d[10]) { date.setSeconds(d[10]); }
+      if (d[12]) { date.setMilliseconds(Number("0." + d[12]) * 1000); }
+      if (d[14]) {
+         offset = (Number(d[16]) * 60) + Number(d[17]);
+         offset *= ((d[15] == '-') ? 1 : -1);
+      }
+
+      offset -= date.getTimezoneOffset();
+      time = (Number(date) + (offset * 60 * 1000));
+      
+      ret = new Date();
+      ret.setTime(Number(time));
+    } else ret = new Date(Date.parse(str));
+    return ret ;
+  },
+  
+  _dates: {},
+
+  _zeropad: function(num) { 
+    return ((num<0) ? '-' : '') + ((num<10) ? '0' : '') + Math.abs(num); 
+  },
+  
+  /** @private - convert a date to a string */
+  from: function(date) { 
+    var ret = this._dates[date.getTime()];
+    if (ret) return ret ; 
+    
+    // figure timezone
+    var zp = this._zeropad,
+        tz = 0-date.getTimezoneOffset()/60;
+        
+    tz = (tz === 0) ? 'Z' : '%@:00'.fmt(zp(tz));
+    
+    this._dates[date.getTime()] = ret = "%@-%@-%@T%@:%@:%@%@".fmt(
+      zp(date.getFullYear()),
+      zp(date.getMonth()+1),
+      zp(date.getDate()),
+      zp(date.getHours()),
+      zp(date.getMinutes()),
+      zp(date.getSeconds()),
+      tz) ;
+    
+    return ret ;
+  }
+});
+
+/** @private */
+SC.RecordAttribute.registerTransform(Django.URLField, {
+  
+  /** @private - 
+    convert a slug field to a string 
+    allow null through as that will be checked separately
+  */
+  to: function(obj) {
+    if (!(typeof obj === SC.T_STRING) && !SC.none(obj) && obj.toString) {
+      obj = obj.toString();
+    }
+    return obj;
+  }
+  
+});
+
+/** @private */
+SC.RecordAttribute.registerTransform(Django.XMLField, {
+  
+  /** @private - 
+    convert an XML field to an XML
+    allow null through as that will be checked separately
+  */
+  to: function(obj) {
+    if (!(typeof obj === SC.T_STRING) && !SC.none(obj) && obj.toString) {
+      obj = obj.toString();
+    }
+    return obj ? Danjgo.parseXML(obj) : '' ;
+  }
+  
+});
+
