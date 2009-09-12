be197b641435c40cdf679bbb92e2e88cd1218536
Erich Ocean <erich.ocean@me.com>
2009-09-12 12:46:01 -0700

    added record attributes

--- models/record_attribute.js
+++ models/record_attribute.js
@@ -50,44 +50,101 @@ SC.RecordAttribute.registerTransform(Django.CommaSeparatedIntegerField, {
   
 });
 
+Django.DATE_FIELD_REGEXP = /([0-9]{4})(-([0-9]{2})(-([0-9]{2})/ ;
+
 /** @private */
-SC.RecordAttribute.registerTransform(Django.DateTimeField, {
+SC.RecordAttribute.registerTransform(Django.DateField, {
 
   /** @private - convert a string to a Date */
   to: function(str, attr) {
     var ret ;
     
-    if (attr.get('useIsoDate')) {
-      var regexp = "([0-9]{4})(-([0-9]{2})(-([0-9]{2})" +
-             "(T([0-9]{2}):([0-9]{2})(:([0-9]{2})(\\.([0-9]+))?)?" +
-             "(Z|(([-+])([0-9]{2}):([0-9]{2})))?)?)?)?",
-          d      = str.match(new RegExp(regexp)),
-          offset = 0,
-          date   = new Date(d[1], 0, 1),
-          time ;
-
-      if (d[3]) { date.setMonth(d[3] - 1); }
-      if (d[5]) { date.setDate(d[5]); }
-      if (d[7]) { date.setHours(d[7]); }
-      if (d[8]) { date.setMinutes(d[8]); }
-      if (d[10]) { date.setSeconds(d[10]); }
-      if (d[12]) { date.setMilliseconds(Number("0." + d[12]) * 1000); }
-      if (d[14]) {
-         offset = (Number(d[16]) * 60) + Number(d[17]);
-         offset *= ((d[15] == '-') ? 1 : -1);
-      }
-
-      offset -= date.getTimezoneOffset();
-      time = (Number(date) + (offset * 60 * 1000));
-      
-      ret = new Date();
-      ret.setTime(Number(time));
-    } else ret = new Date(Date.parse(str));
+    var regexp = "([0-9]{4})(-([0-9]{2})(-([0-9]{2})" +
+           "(T([0-9]{2}):([0-9]{2})(:([0-9]{2})(\\.([0-9]+))?)?" +
+           "(Z|(([-+])([0-9]{2}):([0-9]{2})))?)?)?)?",
+        d      = str.match(new RegExp(regexp)),
+        offset = 0,
+        date   = new Date(d[1], 0, 1),
+        time ;
+        
+    if (d[3]) { date.setMonth(d[3] - 1); }
+    if (d[5]) { date.setDate(d[5]); }
+    if (d[7]) { date.setHours(d[7]); }
+    if (d[8]) { date.setMinutes(d[8]); }
+    if (d[10]) { date.setSeconds(d[10]); }
+    if (d[12]) { date.setMilliseconds(Number("0." + d[12]) * 1000); }
+    if (d[14]) {
+       offset = (Number(d[16]) * 60) + Number(d[17]);
+       offset *= ((d[15] == '-') ? 1 : -1);
+    }
+    
+    offset -= date.getTimezoneOffset();
+    time = (Number(date) + (offset * 60 * 1000));
+    
+    ret = new Date();
+    ret.setTime(Number(time));
+    
     return ret ;
   },
   
   _dates: {},
+  
+  /** @private - convert a date to a string */
+  from: function(date) { 
+    var ret = this._dates[date.getTime()];
+    if (ret) return ret ; 
+    
+    this._dates[date.getTime()] = ret = "%@-%@-%@".fmt(
+      zp(date.getFullYear()),
+      zp(date.getMonth()+1),
+      zp(date.getDate())
+    ) ;
+    
+    return ret ;
+  }
+});
 
+/** @private */
+Django.DATE_TIME_FIELD_REGEXP = new RegExp(
+  "([0-9]{4})(-([0-9]{2})(-([0-9]{2})" +
+  "(T([0-9]{2}):([0-9]{2})(:([0-9]{2})(\\.([0-9]+))?)?" +
+  "(Z|(([-+])([0-9]{2}):([0-9]{2})))?)?)?)?"
+);
+
+/** @private */
+SC.RecordAttribute.registerTransform(Django.DateTimeField, {
+  
+  /** @private - convert a string to a Date */
+  to: function(str, attr) {
+    var ret ;
+    
+    var d      = str.match(Django.DATE_TIME_FIELD_REGEXP),
+        offset = 0,
+        date   = new Date(d[1], 0, 1),
+        time ;
+    
+    if (d[3]) { date.setMonth(d[3] - 1); }
+    if (d[5]) { date.setDate(d[5]); }
+    if (d[7]) { date.setHours(d[7]); }
+    if (d[8]) { date.setMinutes(d[8]); }
+    if (d[10]) { date.setSeconds(d[10]); }
+    if (d[12]) { date.setMilliseconds(Number("0." + d[12]) * 1000); }
+    if (d[14]) {
+       offset = (Number(d[16]) * 60) + Number(d[17]);
+       offset *= ((d[15] == '-') ? 1 : -1);
+    }
+    
+    offset -= date.getTimezoneOffset();
+    time = (Number(date) + (offset * 60 * 1000));
+    
+    ret = new Date();
+    ret.setTime(Number(time));
+    
+    return ret ;
+  },
+  
+  _dates: {},
+  
   _zeropad: function(num) { 
     return ((num<0) ? '-' : '') + ((num<10) ? '0' : '') + Math.abs(num); 
   },
@@ -282,6 +339,9 @@ SC.RecordAttribute.registerTransform(Django.TextField, {
   
 });
 
+/** @private */
+Django.TIME_FIELD_REGEXP = /([0-9]{2}):([0-9]{2}):([0-9]{2})/ ;
+
 /** @private FIXME: How does Django pass times in JSON? */
 SC.RecordAttribute.registerTransform(Django.TimeField, {
 
@@ -289,63 +349,34 @@ SC.RecordAttribute.registerTransform(Django.TimeField, {
   to: function(str, attr) {
     var ret ;
     
-    if (attr.get('useIsoDate')) {
-      var regexp = "([0-9]{4})(-([0-9]{2})(-([0-9]{2})" +
-             "(T([0-9]{2}):([0-9]{2})(:([0-9]{2})(\\.([0-9]+))?)?" +
-             "(Z|(([-+])([0-9]{2}):([0-9]{2})))?)?)?)?",
-          d      = str.match(new RegExp(regexp)),
-          offset = 0,
-          date   = new Date(d[1], 0, 1),
-          time ;
-
-      if (d[3]) { date.setMonth(d[3] - 1); }
-      if (d[5]) { date.setDate(d[5]); }
-      if (d[7]) { date.setHours(d[7]); }
-      if (d[8]) { date.setMinutes(d[8]); }
-      if (d[10]) { date.setSeconds(d[10]); }
-      if (d[12]) { date.setMilliseconds(Number("0." + d[12]) * 1000); }
-      if (d[14]) {
-         offset = (Number(d[16]) * 60) + Number(d[17]);
-         offset *= ((d[15] == '-') ? 1 : -1);
-      }
-
-      offset -= date.getTimezoneOffset();
-      time = (Number(date) + (offset * 60 * 1000));
-      
-      ret = new Date();
-      ret.setTime(Number(time));
-    } else ret = new Date(Date.parse(str));
+    var d      = str.match(Django.TIME_FIELD_REGEXP),
+        offset = 0,
+        date   = new Date(0, 0, 1),
+        time ;
+    
+    date.setHours(d[1]) ;
+    date.setMinutes(d[2]) ;
+    date.setSeconds(d[3]) ;
+    
     return ret ;
   },
   
   _dates: {},
-
-  _zeropad: function(num) { 
-    return ((num<0) ? '-' : '') + ((num<10) ? '0' : '') + Math.abs(num); 
-  },
   
   /** @private - convert a date to a string */
   from: function(date) { 
     var ret = this._dates[date.getTime()];
     if (ret) return ret ; 
     
-    // figure timezone
-    var zp = this._zeropad,
-        tz = 0-date.getTimezoneOffset()/60;
-        
-    tz = (tz === 0) ? 'Z' : '%@:00'.fmt(zp(tz));
-    
-    this._dates[date.getTime()] = ret = "%@-%@-%@T%@:%@:%@%@".fmt(
-      zp(date.getFullYear()),
-      zp(date.getMonth()+1),
-      zp(date.getDate()),
+    this._dates[date.getTime()] = ret = "%@:%@:%@".fmt(
       zp(date.getHours()),
       zp(date.getMinutes()),
       zp(date.getSeconds()),
-      tz) ;
+    );
     
     return ret ;
   }
+  
 });
 
 /** @private */
